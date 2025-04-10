0556507dd473bbbaa31a256cd5e9af512ecbfeeb
Paulo Rodrigues <paulo.jorge.rodrigues@gmail.com>
2025-04-11 00:59:30 +0100

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,41 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('node:http');
+
 function createServer() {
   /* Write your code here */
   // Return instance of http.Server class
+
+  const server = http.createServer((req, res) => {
+    const ret = {
+      parts: [],
+      query: {}
+    };
+
+    const reqArr = req.url.split('?');
+    const parts = reqArr[0].split('/');
+    const params = new URLSearchParams(reqArr[1]);
+
+
+    // get usefull parts
+    const realParts = parts.filter((v) => { return v !== '' });
+    ret.parts = realParts;
+
+    for (const key of params.keys()) {
+      ret.query[key] = params.get(key);
+    }
+
+
+    // headers should be set before sending data
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+
+    res.write(JSON.stringify(ret));
+    res.end();
+  })
+
+  return server;
 }
 
 module.exports = {
